ca0a706ba36046cc9d4a8683f73e1e3e96cb73b6
Marcus Silva <marcusluis.mail@gmail.com>
2023-12-11 16:12:10 -0300

    service and repository

--- adminpanelapi/src/main/java/app/netlify/marcussilva/adminpanelapi/domain/services/EmployeeService.java
+++ adminpanelapi/src/main/java/app/netlify/marcussilva/adminpanelapi/domain/services/EmployeeService.java
@@ -1,5 +1,21 @@
 package app.netlify.marcussilva.adminpanelapi.domain.services;
 
-public interface EmployeeService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import app.netlify.marcussilva.adminpanelapi.domain.entities.EmployeeEntity;
+import app.netlify.marcussilva.adminpanelapi.repositories.EmployeeRepository;
+
+@Service
+public class EmployeeService {
     
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<EmployeeEntity> findAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
 }
--- adminpanelapi/src/main/java/app/netlify/marcussilva/adminpanelapi/repositories/EmployeeRepository.java
+++ adminpanelapi/src/main/java/app/netlify/marcussilva/adminpanelapi/repositories/EmployeeRepository.java
@@ -1,9 +1,13 @@
 package app.netlify.marcussilva.adminpanelapi.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import app.netlify.marcussilva.adminpanelapi.domain.entities.EmployeeEntity;
+
 /**
  * EmployeeRepository
  */
-public class EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 
     
 }
