43f1919cd06a6133a8452dde09479a48865ac2dd
Marcus Silva <marcusluis.mail@gmail.com>
2023-12-12 11:04:25 -0300

    method to get employee by id was created

--- adminpanelapi/src/main/java/app/netlify/marcussilva/adminpanelapi/application/employees/EmployeeController.java
+++ adminpanelapi/src/main/java/app/netlify/marcussilva/adminpanelapi/application/employees/EmployeeController.java
@@ -1,9 +1,13 @@
 package app.netlify.marcussilva.adminpanelapi.application.employees;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +17,20 @@ import app.netlify.marcussilva.adminpanelapi.domain.services.EmployeeService;
 @RestController
 @RequestMapping("api/v1")
 public class EmployeeController {
-    
+
     @Autowired
     private EmployeeService employeeService;
 
     @GetMapping("/employees")
     public List<EmployeeEntity> getAllEmployees() {
         return employeeService.findAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<EmployeeEntity> getEmployeeById(@PathVariable Long id) {
+        Optional<EmployeeEntity> employee = employeeService.findEmployeeById(id);
+        return employee.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
 }
