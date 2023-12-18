9629db1907ebcc8b55747a99c44ca2ef6382e0ee
Marcus Silva <marcusluis.mail@gmail.com>
2023-12-18 20:55:46 -0300

    sidebar started

--- admin-panel-frontend/src/components/Template.tsx
+++ admin-panel-frontend/src/components/Template.tsx
@@ -1,4 +1,5 @@
 import styles from "./Template.module.css"
+import { MdAdminPanelSettings } from "react-icons/md"
 
 interface TemplateProps {
     children: React.ReactNode
@@ -8,6 +9,7 @@ export function Template({ children }: TemplateProps) {
     return (
         <>
             <Header />
+            <Sidebar />
             <div className={styles.GlobalContainer}>{children}</div>
         </>
     )
@@ -17,8 +19,36 @@ function Header() {
     return (
         <div className={styles.HeaderContainer}>
             <header className={styles.Header}>
-                <h3>Painel Administrativo</h3>
+                <div className={styles.HeaderLogo}>
+                    <MdAdminPanelSettings size={38} />
+                    <h3>Painel Administrativo</h3>
+                </div>
             </header>
         </div>
+    )
+}
+
+function Sidebar() {
+    return (
+        <aside className={styles.SidebarContainer}>
+            <div className={styles.SidebarToggle}>
+                <div
+                    className={`${styles.Line} ${styles.LineOne}`}
+                    id="lineOne"
+                ></div>
+                <div
+                    className={`${styles.Line} ${styles.LineTwo}`}
+                    id="lineTwo"
+                ></div>
+                <div
+                    className={`${styles.Line} ${styles.LineThree}`}
+                    id="lineThree"
+                ></div>
+            </div>
+
+            <div>
+                <h3>Dashboard</h3>
+            </div>
+        </aside>
     )
 }
