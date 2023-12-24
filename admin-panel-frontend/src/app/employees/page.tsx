5ec40b82ee16f9278cbe438c6f0dfee9f48e4199
Marcus Silva <marcusluis.mail@gmail.com>
2023-12-24 13:07:47 -0300

    adding styles to emplyee details tag

--- admin-panel-frontend/src/app/employees/page.tsx
+++ admin-panel-frontend/src/app/employees/page.tsx
@@ -53,28 +53,73 @@ export default function EmployeeListPage() {
                 {showForm && <RegisterEmployee onRegister={addEmployee} />}
 
                 {employeeData.map((employee, index) => (
-                    <details key={index}>
-                        <summary>
-                            Nome do funcionário: {employee.firstName}{" "}
-                            {employee.lastName}
+                    <details key={index} className={styles.Details}>
+                        <summary className={styles.Summary}>
+                            <span>Nome do funcionário: </span>
+                            {employee.firstName} {employee.lastName}
                         </summary>
-                        <div>
+                        <div className={styles.DetailsContent}>
                             <p>
-                                Nome completo: {employee.firstName}{" "}
-                                {employee.lastName}
+                                <span>Nome completo: </span>
+                                {employee.firstName} {employee.lastName}
                             </p>
-                            <p>Sexo: {employee.enumGender}</p>
-                            <p>Email: {employee.email}</p>
-                            <p>Data de Nascimento: {employee.birthDate}</p>
-                            <p>RG: {employee.rg}</p>
-                            <p>CPF: {employee.cpf}</p>
-                            <p>Cargo: {employee.enumRole}</p>
-                            <p>Salário: {employee.salary}</p>
-                            <p>CEP: {employee.cep}</p>
-                            <p>Cidade: {employee.localidade}</p>
-                            <p>Estado: {employee.uf}</p>
-                            <p>Data de Admissão: {employee.admissionDate}</p>
-                            <p>Data de Registro: {employee.createdAt}</p>
+                            <p>
+                                <span>Sexo: </span>
+                                {employee.enumGender}
+                            </p>
+                            <p>
+                                <span>Email: </span>
+                                {employee.email}
+                            </p>
+                            <p>
+                                <span>Data de Nascimento: </span>
+                                {employee.birthDate}
+                            </p>
+                            <p>
+                                <span>RG: </span>
+                                {employee.rg}
+                            </p>
+                            <p>
+                                <span>CPF: </span>
+                                {employee.cpf}
+                            </p>
+                            <p>
+                                <span>Cargo: </span>
+                                {employee.enumRole}
+                            </p>
+                            <p>
+                                <span>Salário: </span>
+                                {employee.salary}
+                            </p>
+                            <p>
+                                <span>CEP: </span>
+                                {employee.cep}
+                            </p>
+                            <p>
+                                <span>Cidade: </span>
+                                {employee.localidade}
+                            </p>
+                            <p>
+                                <span>Estado: </span>
+                                {employee.uf}
+                            </p>
+                            <p>
+                                <span>Data de Admissão: </span>
+                                {employee.admissionDate}
+                            </p>
+                            <p>
+                                <span>Data de Registro: </span>
+                                {employee.createdAt}
+                            </p>
+
+                            <div className={styles.ActionButtons}>
+                                <button className={styles.EditButton}>
+                                    Editar
+                                </button>
+                                <button className={styles.DeleteButton}>
+                                    Deletar
+                                </button>
+                            </div>
                         </div>
                     </details>
                 ))}
